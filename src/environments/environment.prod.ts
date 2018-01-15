907fef4d7671ca4b8091fe481345e3d71c666fd9
Matt Smith <matthew.a.smith@rbs.co.uk>
2018-01-15 13:10:02 +0000

    Change API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://collab.rbsres01.net/teams/candidate-attraction-and-engagement-qe9j99bj/app-development/_api'
-
+  api: 'https://collab.rbsres01.net/teams/candidate-attraction-and-engagement-qe9j99bj/_api'
 };
