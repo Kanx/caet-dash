659744db36d8fef0db9f71ae72c9d28d4f860c48
Matt Smith <matthew.a.smith@rbs.co.uk>
2018-01-15 22:21:27 +0000

    F

--- src/app/shared/services/sharepoint.service.ts
+++ src/app/shared/services/sharepoint.service.ts
@@ -63,7 +63,7 @@ export class SharepointService {
   // DOCUMENT OPERATIONS
   getAllFilesAndFolders(folderName: string): Observable<ArrayBuffer> {
     return this.http
-      .get(`${this.api}/web/GetFolderByServerRelativeUrl('Documents')?$expand=Folders,Files`,
+      .get(`${this.api}/web/GetFolderByServerRelativeUrl('Shared Documents')?$expand=Folders,Files`,
       { 'headers': this.headers });
   }
 }
